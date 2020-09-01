722d7741c734e70643c328bdc9c4a24bef695e1d
Tulio <tulioleal13@gmail.com>
2020-09-01 13:06:44 -0300

    Creación de documentos

---
+++ propuesta/easy-bank/src/components/Main.js
@@ -0,0 +1,78 @@
+import React from 'react';
+
+const Main = () =>(
+    <main>
+        <div>
+            <div className='paragrapgh'>
+                <h1></h1>
+                <p></p>
+                <button></button>
+            </div>
+            <div>
+
+            </div>
+        </div>
+
+        <div>
+            <article  className='paragraph'>
+
+            </article>
+            <div>
+                <section className="paragraph">
+                    <img src="" alt=""/>
+                    <h1></h1>
+                    <p></p>
+                    <button></button>
+                </section>
+                <section className="paragraph">
+                    <img src="" alt=""/>
+                    <h1></h1>
+                    <p></p>
+                    <button></button>
+                </section>
+                <section className="paragraph">
+                    <img src="" alt=""/>
+                    <h1></h1>
+                    <p></p>
+                    <button></button>
+                </section>
+                <section className="paragraph">
+                    <img src="" alt=""/>
+                    <h1></h1>
+                    <p></p>
+                    <button></button>
+                </section>
+            </div>
+        </div>
+
+        <div>
+            <h1></h1>
+            <article>
+                <img src="" alt=""/>
+                <span></span>
+                <h1></h1>
+                <p></p>
+            </article>
+            <article>
+                <img src="" alt=""/>
+                <span></span>
+                <h1></h1>
+                <p></p>
+            </article>
+            <article>
+                <img src="" alt=""/>
+                <span></span>
+                <h1></h1>
+                <p></p>
+            </article>
+            <article>
+                <img src="" alt=""/>
+                <span></span>
+                <h1></h1>
+                <p></p>
+            </article>
+        </div>
+    </main>
+)
+
+export default Main
