983197cb1aac3785ae775d0e8ab08cdee3b71f70
Tulio <tulioleal13@gmail.com>
2020-09-02 09:43:10 -0300

    creacion de archivo js para imagenes

---
+++ propuesta/easy-bank/src/assets/images.js
@@ -0,0 +1,28 @@
+import mockUps from "./img/image-mockups.png";
+import currency from "./img/image-currency.jpg";
+import confetti from "./img/image-confetti.jpg";
+import plane from "./img/image-plane.jpg";
+import restaurant from "./img/image-restaurant.jpg";
+
+export default {
+    "mockUps" : {
+        "imagen" : mockUps,
+        "alt" : "Celulares"
+    },
+    "currency" : {
+        "imagen" : currency,
+        "alt" : "Dinero"
+    },
+    "confetti" : {
+        "imagen" : confetti,
+        "alt" : "Confetti"
+    },
+    "plane" : {
+        "imagen" : plane,
+        "alt" : "Avión"
+    },
+    "restaurant" : {
+        "imagen" : restaurant,
+        "alt" : "Restaurante"
+    }
+}
