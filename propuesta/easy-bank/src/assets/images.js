9d2f6ae227af0fee7321323e5d67166f2b81966d
Tulio <tulioleal13@gmail.com>
2020-09-02 17:25:32 -0300

    creacion de objeto para imagenes

--- propuesta/easy-bank/src/assets/images.js
+++ propuesta/easy-bank/src/assets/images.js
@@ -1,8 +1,26 @@
+//jpg y png
 import mockUps from "./img/image-mockups.png";
 import currency from "./img/image-currency.jpg";
 import confetti from "./img/image-confetti.jpg";
 import plane from "./img/image-plane.jpg";
 import restaurant from "./img/image-restaurant.jpg";
+
+//logo
+import logo from "./img/logo.svg"
+
+//svg
+import api from "./img/icon-api.svg"
+import budgeting from "./img/icon-budgeting.svg"
+import onboarding from "./img/icon-onboarding.svg"
+import online from "./img/icon-online.svg"
+
+//iconos
+import facebook from "./img/icon-facebook.svg"
+import instagram from "./img/icon-instagram.svg"
+import pinterest from "./img/icon-pinterest.svg"
+import twitter from "./img/icon-twitter.svg"
+import youtube from "./img/icon-youtube.svg"
+
 
 export default {
     "mockUps" : {
@@ -24,5 +42,45 @@ export default {
     "restaurant" : {
         "imagen" : restaurant,
         "alt" : "Restaurante"
+    },
+    "api":{
+        "imagen" : api,
+        "alt" : "Circuitos"
+    },
+    "budgeting":{
+        "imagen" : budgeting,
+        "alt" : "Ahorro"
+    },
+    "onboarding":{
+        "imagen" : onboarding,
+        "alt" : "Reunion"
+    },
+    "online":{
+        "imagen" : online,
+        "alt" : "Compras online"
+    },
+    "facebook":{
+        "imagen" : facebook,
+        "alt" : "facebook"
+    },
+    "instagram":{
+        "imagen" : instagram,
+        "alt" : "instagram"
+    },
+    "pinterest":{
+        "imagen" : pinterest,
+        "alt" : "pinterest"
+    },
+    "twitter":{
+        "imagen" : twitter,
+        "alt" : "twitter"
+    },
+    "youtube":{
+        "imagen" : youtube,
+        "alt" : "youtube"
+    },
+    "logo":{
+        "imagen" : logo,
+        "alt" : "easy-bank"
     }
 }
